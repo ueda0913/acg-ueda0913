4a13f6144bddcb3ee3895eaa81a6690626d034a7
ueda0913 <yudy.u.1205.kaijo.noa@gmail.com>
2024-04-19 22:53:08 +0900

    finish task01

--- task01/main.cpp
+++ task01/main.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <vector>
 #include <filesystem>
+#include <cmath>
 //
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
@@ -14,7 +15,8 @@
 float area_of_a_triangle(
     float x0, float y0,
     float x1, float y1,
-    float x2, float y2) {
+    float x2, float y2)
+{
   return ((x1 - x2) * (y0 - y2) - (x0 - x2) * (y1 - y2)) * 0.5f;
 }
 
@@ -26,15 +28,19 @@ void draw_triangle(
     float x1, float y1,
     float x2, float y2,
     std::vector<unsigned char> &img_data, unsigned int width, unsigned int height,
-    unsigned char brightness) {
-  for (unsigned int ih = 0; ih < height; ++ih) {
-    for (unsigned int iw = 0; iw < width; ++iw) {
-      const auto x = (float) iw + 0.5f;
-      const auto y = (float) ih + 0.5f;
+    unsigned char brightness)
+{
+  for (unsigned int ih = 0; ih < height; ++ih)
+  {
+    for (unsigned int iw = 0; iw < width; ++iw)
+    {
+      const auto x = (float)iw + 0.5f;
+      const auto y = (float)ih + 0.5f;
       const auto a01 = area_of_a_triangle(x, y, x0, y0, x1, y1);
       const auto a12 = area_of_a_triangle(x, y, x1, y1, x2, y2);
       const auto a20 = area_of_a_triangle(x, y, x2, y2, x0, y0);
-      if (a01 > 0.f && a12 > 0.f && a20 > 0.f) {
+      if (a01 > 0.f && a12 > 0.f && a20 > 0.f)
+      {
         img_data[ih * height + iw] = brightness;
       }
     }
@@ -49,14 +55,18 @@ void draw_triangle(
 void draw_polygon(
     const std::vector<float> &polygon_xy,
     std::vector<unsigned char> &img_data, unsigned int width, unsigned int height,
-    unsigned int brightness) {
-  for (unsigned int ih = 0; ih < height; ++ih) {
-    for (unsigned int iw = 0; iw < width; ++iw) {
+    unsigned int brightness)
+{
+  for (unsigned int ih = 0; ih < height; ++ih)
+  {
+    for (unsigned int iw = 0; iw < width; ++iw)
+    {
       const auto x = float(iw) + 0.5f; // x-coordinate of the center of the pixel
       const auto y = float(ih) + 0.5f; // y-coordinate of the center of the pixel
       const unsigned int num_vtx = polygon_xy.size() / 2;
       float winding_number = 0.0;
-      for (unsigned int iedge = 0; iedge < num_vtx; ++iedge) {
+      for (unsigned int iedge = 0; iedge < num_vtx; ++iedge)
+      {
         unsigned int i0_vtx = iedge;
         unsigned int i1_vtx = (iedge + 1) % num_vtx;
         // positions of the end points of the edge relative to (x,y)
@@ -65,10 +75,20 @@ void draw_polygon(
         float p1x = polygon_xy[i1_vtx * 2 + 0] - x;
         float p1y = polygon_xy[i1_vtx * 2 + 1] - y;
         // write a few lines of code to compute winding number (hint: use atan2)
+        float norm_mul = sqrtf(powf(p0x, 2.0) + powf(p0y, 2.0)) * sqrtf(powf(p1x, 2.0) + powf(p1y, 2.0));
+        float cos_ang = (p0x * p1x + p0y * p1y) / norm_mul;
+        float sin_ang = (p0x * p1y - p0y * p1x) / norm_mul;
+        winding_number += atan2(sin_ang, cos_ang) / 2 / M_PI;
       }
-      const int int_winding_number = int(std::round(winding_number));
-      if (int_winding_number == 1 ) { // if (x,y) is inside the polygon
-        img_data[ih*width + iw] = brightness;
+      int int_winding_number = int(std::round(winding_number)) % 2;
+      if (int_winding_number < 0)
+      {
+        int_winding_number += 2;
+      }
+      // printf("%d\n", int_winding_number);
+      if (int_winding_number == 1)
+      { // if (x,y) is inside the polygon
+        img_data[ih * width + iw] = brightness;
       }
     }
   }
@@ -87,35 +107,67 @@ void dda_line(
     float x1, float y1,
     std::vector<unsigned char> &img_data,
     unsigned int width,
-    unsigned char brightness ) {
+    unsigned char brightness)
+{
   auto dx = x1 - x0;
   auto dy = y1 - y0;
   // write some code below to paint pixel on the line with color `brightness`
+  if (std::abs(dy) <= std::abs(dx))
+  {
+    float m = dy / dx;
+    if (x1 < x0)
+    {
+      std::swap(x0, x1);
+      std::swap(y0, y1);
+    }
+    for (int i = 0; i < int(x1) - int(x0) + 1; ++i)
+    {
+      const int y_index = int(y0 + m * (i));
+      const int x_index = int(x0) + i;
+      img_data[width * y_index + x_index] = brightness;
+    }
+  }
+  else
+  {
+    float m = dx / dy;
+    if (y1 < y0)
+    {
+      std::swap(x0, x1);
+      std::swap(y0, y1);
+    }
+    for (int i = 0; i < int(y1) - int(y0) + 1; ++i)
+    {
+      const int x_index = int(x0 + m * (i));
+      const int y_index = int(y0) + i - 1;
+      img_data[width * y_index + x_index] = brightness;
+    }
+  }
 }
 
-int main() {
+int main()
+{
   constexpr unsigned int width = 100;
   constexpr unsigned int height = 100;
   std::vector<unsigned char> img_data(width * height, 255); // white initial image
   draw_triangle(5., 5., 15., 45., 45., 15., img_data, width, height, 128);
   draw_polygon(
-      {55., 5.,75., 75.,15., 55.,15., 95.,95., 95.,95., 5.}, // the pix coordinates of the corners of the polygon
-      img_data, width, height, // image data, size
-      200); // brightness to draw
-  dda_line(50.0, 50.0, 72.8, 10.0, img_data, width, 0); // right-down
-  dda_line(50.0, 50.0, 90.0, 27.2, img_data, width, 0); // right-down
-  dda_line(50.0, 50.0, 72.8, 90.0, img_data, width, 0); // right-up
-  dda_line(50.0, 50.0, 90.0, 72.8, img_data, width, 0); // right-up
-  dda_line(50.0, 50.0, 10.0, 72.8, img_data, width, 0); // left-up
-  dda_line(50.0, 50.0, 27.2, 90.0, img_data, width, 0); // left-up
-  dda_line(50.0, 50.0, 10.0, 27.2, img_data, width, 0); // left-down
-  dda_line(50.0, 50.0, 27.2, 10.0, img_data, width, 0); // left-down
-  dda_line(50.0, 50.0, 90.0, 50.0, img_data, width, 0); // right
-  dda_line(50.0, 50.0, 50.0, 90.0, img_data, width, 0); // up
-  dda_line(50.0, 50.0, 10.0, 50.0, img_data, width, 0); // left
-  dda_line(50.0, 50.0, 50.0, 10.0, img_data, width, 0); // down
+      {55., 5., 75., 75., 15., 55., 15., 95., 95., 95., 95., 5.}, // the pix coordinates of the corners of the polygon
+      img_data, width, height,                                    // image data, size
+      200);                                                       // brightness to draw
+  dda_line(50.0, 50.0, 72.8, 10.0, img_data, width, 0);           // right-down
+  dda_line(50.0, 50.0, 90.0, 27.2, img_data, width, 0);           // right-down
+  dda_line(50.0, 50.0, 72.8, 90.0, img_data, width, 0);           // right-up
+  dda_line(50.0, 50.0, 90.0, 72.8, img_data, width, 0);           // right-up
+  dda_line(50.0, 50.0, 10.0, 72.8, img_data, width, 0);           // left-up
+  dda_line(50.0, 50.0, 27.2, 90.0, img_data, width, 0);           // left-up
+  dda_line(50.0, 50.0, 10.0, 27.2, img_data, width, 0);           // left-down
+  dda_line(50.0, 50.0, 27.2, 10.0, img_data, width, 0);           // left-down
+  dda_line(50.0, 50.0, 90.0, 50.0, img_data, width, 0);           // right
+  dda_line(50.0, 50.0, 50.0, 90.0, img_data, width, 0);           // up
+  dda_line(50.0, 50.0, 10.0, 50.0, img_data, width, 0);           // left
+  dda_line(50.0, 50.0, 50.0, 10.0, img_data, width, 0);           // down
   // save gray scale image with 1 byte depth
   stbi_write_png(
       (std::filesystem::path(PROJECT_SOURCE_DIR) / "output.png").string().c_str(),
-      width, height, 1,img_data.data(), width);
+      width, height, 1, img_data.data(), width);
 }
