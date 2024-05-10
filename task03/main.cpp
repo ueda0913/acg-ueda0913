4a7e249eddb01a96d17bcc1d32fad01c3a066a9d
ueda0913 <yudy.u.1205.kaijo.noa@gmail.com>
2024-05-10 03:48:40 +0000

    finish task03

--- task03/main.cpp
+++ task03/main.cpp
@@ -63,13 +63,20 @@ void draw_3d_triangle_with_texture(
       const float area1 = (r2 - s).cross(r0 - s);
       const float area2 = (r0 - s).cross(r1 - s);
       if (area0 < 0. || area1 < 0. || area2 < 0.) { continue; } // the pixel is outside the triangle (r0, r1, r2)
-      Eigen::Vector3f bc = Eigen::Vector3f(area0, area1, area2) / (area0 + area1 + area2); // barycentric coordinate on screen
+      // Eigen::Vector3f bc = Eigen::Vector3f(area0, area1, area2) / (area0 + area1 + area2); // barycentric coordinate on screen
       // `bc` gives the barycentric coordinate **on the screen** and it is distorted.
       // Compute the barycentric coordinate ***on the 3d triangle** below that gives the correct texture mapping.
       // (Hint: formulate a linear system with 4x4 coefficient matrix and solve it to get the barycentric coordinate)
       Eigen::Matrix4f coeff;
       Eigen::Vector4f rhs;
-
+      Eigen::Vector3f bc;
+      coeff << q0[0], q1[0], q2[0], -s[0],
+               q0[1], q1[1], q2[1], -s[1],
+               q0[3], q1[3], q2[3], -1.0,
+               1.0, 1.0, 1.0, 0;
+      rhs << 0, 0, 0, 1;
+      auto bc4d = coeff.partialPivLu().solve(rhs);
+      bc = bc4d.head<3>();
       // do not change below
       auto uv = uv0 * bc[0] + uv1 * bc[1] + uv2 * bc[2]; // uv coordinate of the pixel
       // compute pixel coordinate of the texture
